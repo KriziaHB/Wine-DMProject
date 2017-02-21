92153d6865ee97cd6526a873bdcb65640dc6326f
Christopher Moore <moore.christopher515@gmail.com>
2017-02-21 09:00:11 -0600

    Updated FuzzyC Class from Test branch

--- WineFuzzy/WineFuzzy/FuzzyC.h
+++ WineFuzzy/WineFuzzy/FuzzyC.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <vector>
+using namespace std;
 class FuzzyC
 {
+private:
+	int clusters;
+	vector<vector<double>> distance_data;
 public:
-	FuzzyC();
+	FuzzyC(int clusters, vector<vector<double>> &data);
 	~FuzzyC();
 };
 
